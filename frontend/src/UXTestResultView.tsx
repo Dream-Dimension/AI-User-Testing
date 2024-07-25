53d594da6f4aef4fc971e80f4da70eb58bf50119
Dream Dimension <dreamdimension.contact@gmail.com>
2024-07-24 20:19:22 -0500

    expand designs

--- frontend/src/UXTestResultView.tsx
+++ frontend/src/UXTestResultView.tsx
@@ -15,6 +15,9 @@ const UXTestResultView: React.FC<Props> = ({ uxTestResult }) => {
     // State to manage which questions are open
     const [openQuestions, setOpenQuestions] = useState<Set<number>>(new Set(uxTestResult.responses.map((_, index) => index)));
 
+    // State to manage the expansion of designs
+    const [designsAreExpanded, setDesignsAreExpanded] = useState(false);
+
     // Function to toggle the visibility of a question's response
     const toggleQuestion = (index: number) => {
         setOpenQuestions(prevOpenQuestions => {
@@ -28,23 +31,36 @@ const UXTestResultView: React.FC<Props> = ({ uxTestResult }) => {
         });
     };
 
+    // Function to toggle the expansion of designs
+    const toggleExpansion = () => {
+        setDesignsAreExpanded(!designsAreExpanded);
+    };
+
     return (
         <div className='main'>
             <h3>{uxTestResult.scriptName} </h3>
             <p>Conducted on: {moment(uxTestResult.timestampEnd).format('MMMM D, YYYY, h:mm:ss A')}</p>
 
+
             <h1>Designs Analyzed:</h1>
+            <button className='secondary-button' onClick={toggleExpansion}>
+                {designsAreExpanded ? 'Collapse Designs' : 'Expand Designs'}
+            </button>
+            <br />
             {uxTestResult.media.map((mediaPath, index) => {
                 const normalizedPath = mediaPath.replace(/\\/g, '/');
                 const fullUrl = `${SERVER_URL}/${normalizedPath}`;
                 return (
-                    <img key={mediaPath} className='preview-image side-by-side' src={fullUrl} alt={`Preview ${index + 1}`} />
+                    <img
+                        key={mediaPath}
+                        className={`preview-image side-by-side ${designsAreExpanded ? 'expanded' : ''}`}
+                        src={fullUrl}
+                        alt={`Preview ${index + 1}`}
+                    />
                 );
             })}
             <h1>UX Test Result ({uxTestResult.responses.length} responses):</h1>
             {uxTestResult.responses.map(({ question, response }, index) => (
-
-
                 <div key={`response-${index}`} >
                     <div onClick={() => toggleQuestion(index)} className='secondary-button'>
                         <h2> {index + 1}) {question.text} </h2>
@@ -59,7 +75,6 @@ const UXTestResultView: React.FC<Props> = ({ uxTestResult }) => {
                     )}
                 </div>
             ))}
-
         </div>
     );
 };
